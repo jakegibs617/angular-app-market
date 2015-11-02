bf8ee2406cd1f20c74b5b87460c93f09a47e7459
Jacob <jakegibs617@gmail.com>
2015-11-02 08:54:23 -0500

    initial commit

---
+++ js/controllers/MainController.js
@@ -0,0 +1,22 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.move = {
+    icon: 'img/move.jpg',
+    title: 'MOVE',
+    developer: 'MOVE, Inc.',
+    price: 0.99
+  };
+
+  $scope.shutterbugg = {
+    icon: 'img/shutterbugg.jpg',
+    title: 'Shutterbugg',
+    developer: 'Chico Dusty',
+    price: 2.99
+  };
+
+  $scope.gameboard = {
+    icon: 'img/gameboard.jpg',
+    title: 'Gameboard',
+    developer: 'Armando P.',
+    price: 1.99
+  };
+}]);
