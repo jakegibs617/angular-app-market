1811963fdeb9eb42b38349aff237e1566c3583f9
Jacob <jakegibs617@gmail.com>
2015-11-03 13:33:20 -0500

    fixed bug

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # angular-app-market
 
-![app](http://i.imgur.com/JmC2I5y.png)
+![app](http://i.imgur.com/idevPwS.png)
+
+
+to run
+
+$ cd your-app-name
+$ python -m SimpleHTTPServer
+visit http://localhost:8000/
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,22 +1,28 @@
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.move = {
-    icon: 'img/move.jpg',
-    title: 'MOVE',
-    developer: 'MOVE, Inc.',
-    price: 0.99
-  };
-
-  $scope.shutterbugg = {
-    icon: 'img/shutterbugg.jpg',
-    title: 'Shutterbugg',
-    developer: 'Chico Dusty',
-    price: 2.99
-  };
-
-  $scope.gameboard = {
-    icon: 'img/gameboard.jpg',
-    title: 'Gameboard',
-    developer: 'Armando P.',
-    price: 1.99
-  };
+  $scope.apps = [ 
+    { 
+      icon: 'img/move.jpg', 
+      title: 'MOVE', 
+      developer: 'MOVE, Inc.', 
+      price: 0.99 
+    }, 
+    { 
+      icon: 'img/shutterbugg.jpg', 
+      title: 'Shutterbugg', 
+      developer: 'Chico Dusty', 
+      price: 2.99 
+    },
+    {
+      icon: 'img/gameboard.jpg',
+      title: 'Gameboard',
+      developer: 'Armando P.',
+      price: 1.99
+    },
+    {
+      icon: 'img/forecast.jpg',
+      title: 'Forecast',
+      developer: 'Forecast',
+      price: 1.99
+    }
+  ];
 }]);
